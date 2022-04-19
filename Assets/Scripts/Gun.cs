9d4072d77f63c68b52af6d77ab15026dad257066
Leasky02 <adleask@hotmail.co.uk>
2022-04-19 13:03:12 +0100

    Tilemap, Bullet Fix & Scene Creation
    - Added Neptune (scene)
    - Added Saturn (scene)
    - Added tilemap into all scenes
    - Imported Wall tiles
    - Edited 'Bullet' and 'Gun' script

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -6,6 +6,13 @@ public class Gun : MonoBehaviour
 {
     //playerID
     [SerializeField] private int playerID;
+
+    //fire rate
+    [SerializeField] private float fireRate;
+    private float nextTimeToFire = 0f;
+    //damage
+    [SerializeField] private int damage;
+
     //spawn position
     [SerializeField] private Transform bulletSpawnPosition;
     //bullet prefab
@@ -15,8 +22,9 @@ public class Gun : MonoBehaviour
     void Update()
     {
         //if shoot button pressed
-        if(Input.GetButtonDown("Shoot" + playerID))
+        if(Input.GetButton("Shoot" + playerID) && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             //spawn bullet at the spawn position
             var myBullet = Instantiate(bulletPrefab, bulletSpawnPosition.position, Quaternion.identity);
             //call bullet to shoot after spawning with self as parameter
